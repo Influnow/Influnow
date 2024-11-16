75d0f6a373cf4c558f443434ae3ae95a0c2177c0
influnow <influnow2000@gmail.com>
2024-11-16 22:54:46 +0100

    Corrige duplicación de datos y actualiza lógica de inserción

--- set_admin.py
+++ set_admin.py
@@ -12,16 +12,3 @@ if usuario:
 else:
     print("Usuario no encontrado")
 
-from app import db, User  # Importar la base de datos y el modelo de usuario desde tu aplicación
-
-# Buscar al usuario por correo electrónico
-usuario = User.query.filter_by(email="4740vilo@gmail.com").first()
-
-# Actualizar para que sea administrador
-if usuario:
-    usuario.es_administrador = True
-    db.session.commit()
-    print("Usuario actualizado a administrador")
-else:
-    print("Usuario no encontrado")
-
